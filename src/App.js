69b54618c96b694aab823bc5333c5d5b9f02c01f
Michael <optexyz@gmail.com>
2024-08-13 12:21:21 -0400

    styling changes

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
     axios.get('https://api.dictionaryapi.dev/api/v2/entries/en/' + word)
     .then(response => {
       setResult(response.data[0])
+      console.log(response.data)
       setError(false)
     })
     .catch(error => {
@@ -27,27 +28,33 @@ function App() {
 
   return (
     <div className="App">
-        
-     <input type='text' placeholder='Enter word' value={word} onChange={handleWordChange}></input>
-     <button type='submit' onClick={handleSubmit}>Submit</button>
+    <div className='searchContainer'>
+     <input type='text' placeholder='Enter a word' value={word} onChange={handleWordChange} className='searchBox'></input>
+     <button type='submit' onClick={handleSubmit} className='submitButton'><b>Search</b></button>
+     </div>
+     <div className='resultContainer'>
      { !error  ? 
      <div>
       {result != null ? 
       
-      <div>
+      <div className='definitionContainer'>
         <h2>{result.word} ({result.phonetic})</h2>
         {
           result.meanings.map((meaning, index) => {
             return (
               <div>
               <p><b>{meaning.partOfSpeech}</b></p>
+              <ul>
               {
                 meaning.definitions.map((definition, index) => {
                   return (
+                    <li>
                     <p key={index}>{definition.definition}</p>
+                    </li>
                   )
                 })
               }  
+              </ul>
               </div>
             )
           })
@@ -56,12 +63,13 @@ function App() {
       
       
       
-      : <h1>Enter word a word to view its data!</h1>
+      : <h1>Enter word a word to view its meanings!</h1>
       
       }
       </div>
     : <h1>Word could not be found. Try again!</h1>
     }
+    </div>
      
     </div>
   );
