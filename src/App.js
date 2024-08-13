423e55f85e35c643ebfe7342a5e1696c4c8e8dae
Michael <optexyz@gmail.com>
2024-08-12 21:34:06 -0400

    app skeleton

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [word, setWord] = useState("")
+  const [definition, setDefinition] = useState("")
+
+  
+
+  const handleSubmit = () => {
+      console.log("fetching word: " + word)
+  }
+
+  const handleWordChange = (e) => {
+    setWord(e.target.value)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        
+     <input type='text' placeholder='Enter word' value={word} onChange={handleWordChange}></input>
+     <button type='submit' onClick={handleSubmit}>Submit</button>
+      {definition.length > 0 ? <div><h6>{definition}</h6> </div> : <></>} 
+
+
+     
     </div>
   );
 }
