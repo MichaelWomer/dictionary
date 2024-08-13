317505b35bb042679fe4cd91ebabdaeada0c8825
Michael <optexyz@gmail.com>
2024-08-12 22:34:21 -0400

    dictionary api integration

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
-
+import axios from 'axios';
 function App() {
 
   const [word, setWord] = useState("")
-  const [definition, setDefinition] = useState("")
+  const [result, setResult] = useState(null)
+  const [error, setError] = useState(false)
 
   
 
   const handleSubmit = () => {
-      console.log("fetching word: " + word)
+    axios.get('https://api.dictionaryapi.dev/api/v2/entries/en/' + word)
+    .then(response => {
+      setResult(response.data[0])
+      setError(false)
+    })
+    .catch(error => {
+        setError(true)
+    });
   }
 
   const handleWordChange = (e) => {
@@ -22,9 +30,38 @@ function App() {
         
      <input type='text' placeholder='Enter word' value={word} onChange={handleWordChange}></input>
      <button type='submit' onClick={handleSubmit}>Submit</button>
-      {definition.length > 0 ? <div><h6>{definition}</h6> </div> : <></>} 
-
-
+     { !error  ? 
+     <div>
+      {result != null ? 
+      
+      <div>
+        <h2>{result.word} ({result.phonetic})</h2>
+        {
+          result.meanings.map((meaning, index) => {
+            return (
+              <div>
+              <p><b>{meaning.partOfSpeech}</b></p>
+              {
+                meaning.definitions.map((definition, index) => {
+                  return (
+                    <p key={index}>{definition.definition}</p>
+                  )
+                })
+              }  
+              </div>
+            )
+          })
+        }
+      </div>
+      
+      
+      
+      : <h1>Enter word a word to view its data!</h1>
+      
+      }
+      </div>
+    : <h1>Word could not be found. Try again!</h1>
+    }
      
     </div>
   );
